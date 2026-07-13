7b46496aa32988c3340fc6fcb32a26fe425dbac1
saguan0922-ops <saguan0922@gmail.com>
2026-07-13 19:23:05 +0900

    Update courses.js

--- courses.js
+++ courses.js
@@ -10,7 +10,7 @@ window.DEEPFRAME_COURSES = [
     label: "초1학년-5,6차시",
     cover: ["#555555", "#111111", "rgba(255,255,255,.38)"]
   },
-  { id: "VIDEO_ID_2", title: "2강 제목", duration: "00분", label: "교육영상", cover: ["#35404a","#0d1114","rgba(170,210,255,.35)"] },
+  { id: "a9ad600d13504c9b8f3858b9f4333f35", title: "초등 2학년", duration: "29분 11초", label: "초2학년-3,4차시", cover: ["#35404a","#0d1114","rgba(170,210,255,.35)"] },
   { id: "VIDEO_ID_3", title: "3강 제목", duration: "00분", label: "교육영상", cover: ["#493b36","#120e0d","rgba(255,184,150,.32)"] },
   { id: "VIDEO_ID_4", title: "4강 제목", duration: "00분", label: "교육영상", cover: ["#3d354b","#100d14","rgba(210,175,255,.32)"] },
   { id: "VIDEO_ID_5", title: "5강 제목", duration: "00분", label: "교육영상", cover: ["#34453e","#0d120f","rgba(170,255,209,.28)"] },
