c92cd16e073565a89dcb7f60ee8b87a8ffc222c6
saguan0922-ops <saguan0922@gmail.com>
2026-07-13 19:35:07 +0900

    Update courses.js

--- courses.js
+++ courses.js
@@ -12,9 +12,9 @@ window.DEEPFRAME_COURSES = [
   },
   { id: "a9ad600d13504c9b8f3858b9f4333f35", title: "초등 2학년", duration: "29분 11초", label: "초2학년-3,4차시", cover: ["#35404a","#0d1114","rgba(170,210,255,.35)"] },
   { id: "a7215a410fab40358469bf595550132c", title: "초등 3학년", duration: "1시간 30분 31초", label: "전차시", cover: ["#493b36","#120e0d","rgba(255,184,150,.32)"] },
-  { id: "VIDEO_ID_4", title: "4강 제목", duration: "00분", label: "교육영상", cover: ["#3d354b","#100d14","rgba(210,175,255,.32)"] },
-  { id: "VIDEO_ID_5", title: "5강 제목", duration: "00분", label: "교육영상", cover: ["#34453e","#0d120f","rgba(170,255,209,.28)"] },
-  { id: "VIDEO_ID_6", title: "6강 제목", duration: "00분", label: "교육영상", cover: ["#4a4534","#12110c","rgba(255,231,160,.28)"] },
-  { id: "VIDEO_ID_7", title: "7강 제목", duration: "00분", label: "교육영상", cover: ["#3b3b3b","#101010","rgba(255,255,255,.26)"] },
+  { id: "d39d648e5a08405597e8b273fb3344c9", title: "7세", duration: "1시간 56분", label: "말놀이 숲에서 책이랑 쑥쑥", cover: ["#3d354b","#100d14","rgba(210,175,255,.32)"] },
+  { id: "a3c34c175c82402d9d4f0a92206dbc21", title: "초등 1학년", duration: "2시간 05분 1초", label: "학교가는 책 친구, 도서관에서 레벨업!", cover: ["#34453e","#0d120f","rgba(170,255,209,.28)"] },
+  { id: "469174a9622641319781d68f34149472", title: "초등 2학년", duration: "2시간 13분 57초", label: "책탐정 본부 : 읽고, 찾고, 연결해라!", cover: ["#4a4534","#12110c","rgba(255,231,160,.28)"] },
+  { id: "e25c5e1bec69428592ab6a6fb640b2bd", title: "초등 3학년", duration: "1시간 57분", label: "도서관 뉴스랩 : 읽고, 엮고, 말하라!", cover: ["#3b3b3b","#101010","rgba(255,255,255,.26)"] },
   { id: "VIDEO_ID_8", title: "8강 제목", duration: "00분", label: "교육영상", cover: ["#3d3440","#110d12","rgba(255,174,229,.27)"] }
 ];
